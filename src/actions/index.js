39cfb32f356355b0c37de316a8a611547d2d1cd2
Giovanna Betti <giovannabetti@gmail.com>
2021-03-03 15:05:06 -0300

    cute and puuurfect cats api

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,4 +7,16 @@ export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
   // Desenvolva aqui o código da action assíncrona
-}
+  return async (dispatch) => {
+    try {
+      dispatch(requestAPI());
+
+      const response = await fetch('https://aws.random.cat/meow');
+      const data = await response.json();
+
+      dispatch(getPicture(data));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
